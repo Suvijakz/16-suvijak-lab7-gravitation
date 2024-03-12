581363685d98de4af7194fe559cc3ec4242f404e
Suvijakz <suvijak.pita@bumail.net>
2024-03-12 10:01:22 +0700

    update

--- Assets/Attractor.cs
+++ Assets/Attractor.cs
@@ -4,10 +4,23 @@ using UnityEngine;
 
 public class Attractor : MonoBehaviour
 {
-    [SerializeField] Rigidbody rb;
+    [SerializeField] Rigidbody rb; 
+    float G = 6.674f;
 
     void Attract(Attractor other)
     {
         Rigidbody rb2 = other.rb;
+        
+        //F = G(m1*m2)/r^2
+        Vector3 direction = rb.position - rb2.position;
+
+        float distance = direction.magnitude;
+
+        float forceMagnitude = G * (rb.mass * rb2.mass) / Mathf.Pow(distance,2);
+
+        Vector3 finalForce = forceMagnitude * direction.normalized;
+        
+        rb2.AddForce(finalForce);
     }
+    
 }
